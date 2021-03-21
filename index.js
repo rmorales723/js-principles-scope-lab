fa862ecc1077c12959fd8cfcb8d36fa8f1627af3
robert morales <rmoralfit@gmail.com>
2021-03-21 11:49:11 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName = 'bob'
+const leastFavoriteCustomer = 'rob'
+git add .add
+function customerName(bob) {
+    return (customerName);
+}
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+ return bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    return bestCustomer = 'maybe bob';
+   }
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'rob';
+   }
