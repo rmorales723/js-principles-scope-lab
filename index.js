efe0f0171404ae23ee6fee51ac3f100ab536ee0b
robert morales <rmoralfit@gmail.com>
2021-03-21 11:48:24 -0400

    commit tree ca9b4de9799b32ba3753739a0b2778ea3b54fd77

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = 'bob'
+const leastFavoriteCustomer = 'rob'
+function customerName(bob) {
+    return (customerName);
+}
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+ return bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    return bestCustomer = 'maybe bob';
+   }
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'rob';
+   }
